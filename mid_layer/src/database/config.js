b40c66f7241c0b8d5473b26dc788cc40a3409a69
zz <benzzvsjs@gmail.com>
2025-02-09 10:53:21 +1100

    Remove unused import

--- mid_layer/src/database/config.js
+++ mid_layer/src/database/config.js
@@ -1,7 +1,6 @@
 // NOTE: We use mariadb not mysql.
 
 // Set the default timezone. Assume always Australia.
-const process = require("../../.eslintrc");
 const TIME_ZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 const CONFIG = {
